7007dfc7857c3fd02fe352fac09a5da42879d0e9
Piotr Gzubicki <piotr.gzubicki97@gmail.com>
2018-10-29 12:32:27 +0100

    Revert "Kurwa"
    This reverts commit d775d874a2fe1fbf09db0ced0c2752688904b942.

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -11,7 +11,7 @@ namespace Server
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git");
-            Console.WriteLine("Hello");
+            Console.WriteLine("H");
             Console.ReadKey();
         }
     }
