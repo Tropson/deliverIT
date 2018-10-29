41d9150cc268d630c6f66e7b3d97c4371cc9aa7d
David Szoke <tropson90@gmail.com>
2018-10-29 09:55:14 +0100

    asd

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -11,7 +11,7 @@ namespace Server
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git");
-            Console.WriteLine("Hi");
+            Console.WriteLine("H");
             Console.ReadKey();
         }
     }
