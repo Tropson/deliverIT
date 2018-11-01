c20b507f5f296e83daa657799505867cd43471de
David Szoke <tropson90@gmail.com>
2018-11-01 11:06:59 +0100

    Added AddSenderTest

--- Server/UnitTestProject1/UnitTest1.cs
+++ Server/UnitTestProject1/UnitTest1.cs
@@ -17,10 +17,18 @@ namespace UnitTestProject1
             Assert.IsNotNull(proxy);
         }
         [TestMethod]
-        [DataRow()]
-        public void AddSenderTest()
+        [DataRow("2611982375","Dávid","Szőke","91933260","tropson90@gmail.com","Fredensgade 7, 2st","9000","Aalborg")]
+        public void AddSenderTest(string cpr, string firstName, string lastName, string email, string address, string zipCode, string city)
         {
-            
+            //setup
+            var proxy = new ServiceReference1.DeliveryServiceClient();
+            Person person = { Cpr = cpr, FirstName = firstName, LastName = lastName, Email = email, Address = address, ZipCode = zipCode, City = city };
+
+            //addToDB
+            var result = proxy.AddPerson(person);
+
+            //assert
+            Assert.Equals(result, 1);
         }
     }
 }
