d87c4397c698bdd269aa579da2daaccaddc3d1a4
Piotr Gzubicki <piotr.gzubicki97@gmail.com>
2018-11-01 13:18:43 +0100

    small change

--- Server/WebClientMVC/Controllers/SenderController.cs
+++ Server/WebClientMVC/Controllers/SenderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebClientMVC.Models;
 
 namespace WebClientMVC.Controllers
 {
@@ -28,7 +29,7 @@ namespace WebClientMVC.Controllers
 
         // POST: Sender/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(SenderModel sender)
         {
             try
             {
