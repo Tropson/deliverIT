74394f4be224ea620df6609e267435b73e3ee9c8
David Szoke <tropson90@gmail.com>
2018-11-01 10:53:07 +0100

    Added unit test and renamed service

---
+++ Server/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnitTestProject1.ServiceReference1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ServiceConnectionTest()
+        {
+            //setup
+            var proxy = new ServiceReference1.DeliveryServiceClient();
+
+            //assert
+            Assert.IsNotNull(proxy);
+        }
+        [TestMethod]
+        [DataRow()]
+        public void AddSenderTest()
+        {
+            
+        }
+    }
+}
