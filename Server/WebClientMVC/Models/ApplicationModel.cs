e26392daad9a3945e7e34e02062dcbff1bd0ea85
David Szoke <tropson90@gmail.com>
2018-11-05 12:29:22 +0100

    Added applicationModel to client

---
+++ Server/WebClientMVC/Models/ApplicationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebClientMVC.Models
+{
+    public class ApplicationModel:PersonModel
+    {
+        public ApplicationModel(string cpr, string firstName, string lastName, string phone, string email, string address, string zipCode, string city) : base(cpr, firstName, lastName, phone, email, address, zipCode, city)
+        {
+        }
+        public string CVPath { get; set; }
+        public string IDPicturePath { get; set; }
+        public string YellowCardPath { get; set; }
+       
+    }
+}
