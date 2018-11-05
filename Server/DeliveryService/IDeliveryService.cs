c11ba7901303e9c47731c116ec77353e1ede6445
David Szoke <tropson90@gmail.com>
2018-11-05 23:56:06 +0100

    Added [OperationContract]

--- Server/DeliveryService/IDeliveryService.cs
+++ Server/DeliveryService/IDeliveryService.cs
@@ -19,6 +19,7 @@ namespace DeliveryService
         [OperationContract]
         int AddApplication(ApplicationModel application);
 
+        [OperationContract]
         List<ApplicationModel> GetApplications();
 
         [OperationContract]
