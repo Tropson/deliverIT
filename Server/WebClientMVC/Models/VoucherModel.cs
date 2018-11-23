78e9c0d8caa8cb721cab1dd2779a9e8175b0845d
David Szoke <tropson90@gmail.com>
2018-11-23 10:36:44 +0100

    Voucher related stuff

---
+++ Server/WebClientMVC/Models/VoucherModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebClientMVC.Models
+{
+    public class VoucherModel
+    {
+        public string code { get; set; }
+        public int amount { get; set; }
+    }
+}
