c38113709294bdc5addeb5764a8ec551264ebce3
Piotr Gzubicki <piotr.gzubicki97@gmail.com>
2018-11-26 14:08:22 +0100

    Package model

---
+++ Server/WebClientMVC/Models/PackageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebClientMVC.Models
+{
+    public class PackageModel
+    {
+        public int StatusID { get; set; }
+        public int SenderID { get; set; }
+        public int CourierID { get; set; }
+        public string ToAddress { get; set; }
+        public string FromAddress { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+    }
+}
