ba204d3fd58a0453e0dd5314a9d72bdc40a379f9
Piotr Gzubicki <piotr.gzubicki97@gmail.com>
2019-01-06 18:02:27 +0100

    Changed database to kraka because of microsoft strange stuff

--- Server/DeliveryServiceLibrary/Properties/Settings.Designer.cs
+++ Server/DeliveryServiceLibrary/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DeliveryServiceLibrary.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -42,6 +42,17 @@ namespace DeliveryServiceLibrary.Properties {
         public string DeliverITDatabaseConnectionString1 {
             get {
                 return ((string)(this["DeliverITDatabaseConnectionString1"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=kraka.ucn.dk;Initial Catalog=dmaj0917_1067643;User ID=dmaj0917_106764" +
+            "3;Password=Password1!")]
+        public string dmaj0917_1067643ConnectionString {
+            get {
+                return ((string)(this["dmaj0917_1067643ConnectionString"]));
             }
         }
     }
